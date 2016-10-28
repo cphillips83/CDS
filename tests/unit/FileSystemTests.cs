51e3be6733533517ded1be192d701fd50bc9ee7b
Chris Phillips <chris.phillips@mindmatrix.net>
2016-10-28 14:24:25 -0400

    testing file/dir entry operators

--- tests/unit/FileSystemTests.cs
+++ tests/unit/FileSystemTests.cs
@@ -18,6 +18,42 @@ namespace unit
         }
 
         [TestMethod]
+        public void FileEntry_Equals_Operator()
+        {
+            var fe1 = new FileEntry(Hash.Create(1, 2, 3, 4), Hash.Create(4, 3, 2, 1));
+            var fe2 = new FileEntry(Hash.Create(1, 2, 3, 4), Hash.Create(4, 3, 2, 1));
+            
+            Assert.IsTrue(fe1 == fe2);
+        }
+
+        [TestMethod]
+        public void FileEntry_NotEquals_Operator()
+        {
+            var fe1 = new FileEntry(Hash.Create(1, 2, 3, 4), Hash.Create(4, 3, 2, 1));
+            var fe2 = new FileEntry(Hash.Create(1, 2, 3, 4), Hash.Create(4, 3, 2, 1));
+
+            Assert.IsFalse(fe1 != fe2);
+        }
+
+        [TestMethod]
+        public void DirectoryEntry_Equals_Operator()
+        {
+            var de1 = new DirectoryEntry(Hash.Create(1, 2, 3, 4));
+            var de2 = new DirectoryEntry(Hash.Create(1, 2, 3, 4));
+
+            Assert.IsTrue(de1 == de2);
+        }
+
+        [TestMethod]
+        public void DirectoryEntry_NotEquals_Operator()
+        {
+            var de1 = new DirectoryEntry(Hash.Create(1, 2, 3, 4));
+            var de2 = new DirectoryEntry(Hash.Create(1, 2, 3, 4));
+
+            Assert.IsFalse(de1 != de2);
+        }
+
+        [TestMethod]
         public void FileSystem_CompareFile_OK()
         {
             var left = new FileEntry(Hash.Create(1, 2, 3, 4), Hash.Create(4, 3, 2, 1));
